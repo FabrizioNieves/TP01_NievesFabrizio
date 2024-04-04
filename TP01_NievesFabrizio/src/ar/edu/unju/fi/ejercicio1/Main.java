458c027d2a1aa826e4821db727894bf8bded234e
niyuv <niyuv@DESKTOP-NI2GFIU>
2024-04-04 16:09:54 -0300

    punto 1 Fabrizio Nieves

--- TP01_NievesFabrizio/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ TP01_NievesFabrizio/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -2,13 +2,9 @@ package ar.edu.unju.fi.ejercicio1;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
-		int n1 = 8, n2 = 5, n3 = 1, n4 = 20, n5 = 9;
-		double promedio = (double)(n1+n2+n3+n4+n5)/5;
-		
-		System.out.println(n1+" "+ n2 +" "+ n3 +" "+ n4 +" "+ n5);
-		System.out.println("Promedio: "+promedio);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
