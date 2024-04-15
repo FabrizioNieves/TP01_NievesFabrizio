bd78bf9b5eb71975ca4e2f42284390bd3290bd86
niyuv <niyuv@DESKTOP-NI2GFIU>
2024-04-15 18:46:48 -0300

    se realiza el ejercicio 2 Fabrizio Jorge Nieves

---
+++ TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String NomPais = "Australia";
+		int PersonaE = 19;
+		float AltEd = 20.6f;
+		float ProdSuper = 99.99f;
+		double Telefono = 3886866918d;
+		double CalCoseno = Math.cos(0.5);
+		
+		
+		System.out.println("El nombre del pais es: "+NomPais);
+		System.out.println("La edad de una persona: "+PersonaE);
+		System.out.println("La altura de un edificio: "+AltEd);
+		System.out.println("El precio de un producto del Super: "+ProdSuper);
+		System.out.println("Numero de telefono: "+Telefono);
+		System.out.println("El calculo de coseno de 0,5: "+CalCoseno);
+	}
+
+}
