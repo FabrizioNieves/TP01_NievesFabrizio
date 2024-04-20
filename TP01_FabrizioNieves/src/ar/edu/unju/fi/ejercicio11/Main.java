3688ae2e8c5b4ba8c6b6ccada706d4e17fa4a226
niyuv <niyuv@DESKTOP-NI2GFIU>
2024-04-20 20:50:46 -0300

    ejercicio 11 Fabrizio Jorge Nieves

---
+++ TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+int i, j;
+int suma = 0;
+for( i = 2, j = 40; j > 0; j--)
+	{
+		suma = j*i;
+		i++;
+		System.out.printf("%d ", + suma);
+	}
+        
+}}
