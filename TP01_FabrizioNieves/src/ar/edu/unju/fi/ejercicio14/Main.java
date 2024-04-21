92f65d9e1f95edb584d9db3c50942f1bc5f6ce46
niyuv <niyuv@DESKTOP-NI2GFIU>
2024-04-20 21:58:55 -0300

    Ejercicio 14 Fabrizio Jorge Nieves

--- TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio14/Main.java
+++ TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio14/Main.java
@@ -1,10 +1,35 @@
 package ar.edu.unju.fi.ejercicio14;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int numero;
+		do {
+			System.out.print("Ingresar un numero entero dentro de este rango [3, 10]: ");
+			numero = scanner.nextInt();
+			if( numero < 3 || numero > 10 ) {
+			System.out.println("numero fuera del rango, ingresar de nuevo el numero");
+										}
+			}
+		while(numero <= 2 || numero >= 11);
+	int[] array = new int[numero];
+	System.out.println("Ingresar numeros para el array de " +numero+ " posiciones: ");
+	for(int i = 0; i < numero; i++)
+		{
+		array[i] = scanner.nextInt();
+		}
+	int suma = 0;
+	for(int i = 0; i < numero; i++)
+		{
+		suma += array[i];
+		}
+		System.out.print("valores del array: \n");
+	for(int i=0; i<numero; i++)
+		{
+		System.out.println("Posicion: " +i +"     Valor: "+array[i]);
+		}
+		System.out.print("La suma de los valores del array es: "+suma);
 	}
 
 }
