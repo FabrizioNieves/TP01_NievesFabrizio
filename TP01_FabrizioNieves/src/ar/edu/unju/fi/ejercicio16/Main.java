5ff0c8a885c949956496d4714fb371193f2bad30
niyuv <niyuv@DESKTOP-NI2GFIU>
2024-04-20 22:14:22 -0300

    Ejercicio 16 Fabrizio Jorge Nieves

--- TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio16/Main.java
+++ TP01_FabrizioNieves/src/ar/edu/unju/fi/ejercicio16/Main.java
@@ -1,10 +1,36 @@
 package ar.edu.unju.fi.ejercicio16;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        String[] nombres = new String[5];
+    for (int i = 0; i < nombres.length; i++) {
+        System.out.println("Ingresar nombre de la persona " + (i + 1) + ":");
+        nombres[i] = scanner.nextLine();
+        }
 
+    int i = 0;
+    while (i < nombres.length)	{
+            System.out.println("Nombre " + (i + 1) + ": " + nombres[i]);
+            i++;
+        						}
+        System.out.println("Tamaño del array : " + nombres.length);
+     byte indiceEliminar;
+      do{
+    	  System.out.println("Ingresar indice del elemento a eliminar (0-" + (nombres.length - 1) + "):");
+    	  indiceEliminar = scanner.nextByte();
+        } 
+      while (indiceEliminar < 0 || indiceEliminar >= nombres.length);
+        if (indiceEliminar < nombres.length - 1)	{
+            System.arraycopy(nombres, indiceEliminar + 1, nombres, indiceEliminar, nombres.length - indiceEliminar - 1);
+        											}
+        nombres[nombres.length - 1] = "";
+        i = 0;
+       while (i < nombres.length) {
+            System.out.println("Nombre " + (i + 1) + ": " + nombres[i]);
+            i++;
+        }
 	}
 
 }
